5e4e6230390a94559a682adb088abbc7ef839935
koushik-00 <phaneendrakoushik@gmail.com>
2024-01-16 12:15:09 +0530

    patterns

---
+++ striver A2Z/patterns/7.cpp
@@ -0,0 +1,28 @@
+// asterisk pyramid
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int rows;
+    cout<<"Enter the number of rows: ";
+    cin>>rows;
+
+    for (int i=0; i<rows; i++){
+        for (int j=0; j<rows-i-1; j++) {
+            cout<<" ";
+        }
+        for (int k=0; k<2*i+1; k++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
