4396248da7d88f340f042cd47321ca90f1ba61b6
koushik-00 <phaneendrakoushik@gmail.com>
2024-02-01 08:48:41 +0530

    190

--- leetcode/reverse_bits.cpp
+++ leetcode/reverse_bits.cpp
@@ -2,19 +2,31 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+uint32_t reverseBits(uint32_t n) {
+    int res = 0;
+    
+    for ( int i = 0; i < 32; i++ ) {
+        res = res << 1;
+        res = res | ( n & 1 );
+        n = n >> 1;
+    }
+    return res;
+}
+
 int main()
 {
-    uint32_t a, rev=0,temp;
+    uint32_t a,temp;
     cin>>a;
+    int rev = 0;
     temp=a;
 
-    for (int i=0; i<32; i++) {
-        rev=rev << 1;
-        rev=rev | ( a & 1 );
-        a=a >> 1;
-    }
+    // for (int i=0; i<32; i++) {
+    //     rev=rev << 1;
+    //     rev=rev | ( a & 1 );
+    //     a=a >> 1;
+    // }
 
-    cout<<"Reverse of "<<temp<<" : "<<rev<<endl;
+    cout<<"Reverse of "<<temp<<" : "<<reverseBits(a)<<endl;
     cout<<a;
 
 //sample input 00000010100101000001111010011100
